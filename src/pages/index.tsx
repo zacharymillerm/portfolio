0c0e3ad9a8063ac75d1e92938965678bcd4071e6
zacharymiller <zacharymiller0818@gmail.com>
2023-08-22 14:05:40 -0400

    delete some features 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,10 @@ import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
-import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
-import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
 
 // eslint-disable-next-line react-memo/require-memo
@@ -23,8 +21,6 @@ const Home: FC = memo(() => {
       <About />
       <Resume />
       <Portfolio />
-      <Testimonials />
-      <Contact />
       <Footer />
     </Page>
   );
