0f3cb486973d4b01b8749a086edb30435cc41876
zacharymiller <zacharymiller0818@gmail.com>
2023-08-22 11:58:42 -0400

    edite portfolio

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -20,11 +20,12 @@ import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
 // import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-// import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
 // import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 // import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 // import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+// import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import porfolioImage12 from '../images/portfolio/portfolio-12.jpg';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -45,7 +46,7 @@ import {
  */
 export const homePageMeta: HomepageMeta = {
   title: 'Zachary Miller',
-  description: "Example site built with Tim Baker's react resume template",
+  description: "Here, you can know enough about me!",
 };
 
 /**
@@ -69,12 +70,14 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Zachary Miller.`,
+  name: `I'm Zachary Miller`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a <strong className="text-stone-100">full-stack engineer</strong> with 7+ years of experience in <strong className="text-stone-100">Web/Mobile/Blockchain</strong> development and passionate about cutting-edge technologies like ML and blockchain.
-        I'm open to new <strong className="text-stone-100">opportunities</strong> and <strong className="text-stone-100">challenges</strong>.
+        I'm a <strong className="text-stone-100">full-stack engineer</strong> with 7+ years of experience in{' '}
+        <strong className="text-stone-100">Web/Mobile/Blockchain</strong> development and passionate about cutting-edge
+        technologies like ML and blockchain. I'm open to new <strong className="text-stone-100">opportunities</strong>{' '}
+        and <strong className="text-stone-100">challenges</strong>.
       </p>
     </>
   ),
@@ -120,69 +123,69 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'English',
-        level: 10,
-     },
+        level: 9,
+      },
       {
         name: 'Japanese',
-        level: 4,
-     },
+        level: 5,
+      },
       {
         name: 'Spanish',
         level: 2,
-     },
+      },
     ],
- },
+  },
   {
     name: 'Frontend development',
     skills: [
       {
-        name: 'React',
+        name: 'React, Angluar, Vue',
         level: 9,
-     },
+      },
       {
-        name: 'Typescript',
+        name: 'Typescript, Javascript',
         level: 9,
-     },
+      },
       {
-        name: 'GraphQL',
+        name: 'GraphQL, Redux, Restful API',
         level: 7,
-     },
+      },
     ],
- },
+  },
   {
     name: 'Backend development',
     skills: [
       {
-        name: 'Node.js',
+        name: 'Node.js, Express, Nest',
         level: 8,
-     },
+      },
       {
-        name: 'Nest.js',
-        level: 5,
-     },
+        name: 'Django',
+        level: 6,
+      },
       {
         name: 'Golang',
         level: 4,
-     },
+      },
     ],
- },
+  },
   {
     name: 'Mobile development',
     skills: [
       {
         name: 'React Native',
         level: 9,
-     },
+      },
       {
         name: 'Swift',
         level: 6,
-     },
+      },
       {
         name: 'Flutter',
         level: 4,
-     },
+      },
     ],
- },
+  },
 ];
 
 /**
@@ -194,31 +197,37 @@ export const portfolioItems: PortfolioItem[] = [
     description: '',
     url: 'https://liftcannabis.ca/',
     image: porfolioImage1,
- },
+  },
   {
     title: 'Ravi Surya Partnership Program',
     description: '',
     url: 'https://www.ravisuryagroup.com/surya/',
     image: porfolioImage2,
- },
+  },
   {
-    title: 'Online Interior Design Services',
+    title: 'Baby Healthcare Site',
     description: '',
-    url: 'https://www.laurelandwolf.com/',
+    url: 'https://chibebe.com.au/',
     image: porfolioImage4,
- },
+  },
   {
     title: 'PRAGUE AIRPORT',
     description: '',
     url: 'https://www.prague-airport.com/',
     image: porfolioImage5,
- },
+  },
   {
-    title: 'Super-Simple Financial Forecasting Software',
+    title: 'Rent/Buy Books on Chegg',
     description: '',
-    url: 'https://sumosum.com/',
-    image: porfolioImage11,
- },
+    url: 'https://www.chegg.com/books',
+    image: porfolioImage7,
+  },
+  {
+    title: 'Book Top-Rated House Cleaners',
+    description: '',
+    url: 'https://www.instamaids.com/',
+    image: porfolioImage12,
+  },
 ];
 
 /**
@@ -228,42 +237,39 @@ Texas Tech University
 Bachelor of Science - BS, Computer Science
  */
 
-
 export const education: TimelineItem[] = [
   {
     date: 'April 2015',
     location: 'Texas Tech University',
     title: 'Bachelor of Science(BS) in Computer Science',
     content: <p></p>,
- },
+  },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'Feb 2018 - May 2023',
-    location: 'Awesome Development Company',
+    date: 'Feb 2018 - Present',
+    location: 'Awesome Development Company | Remote',
     title: 'Senior UX Engineer',
     content: (
       <div>
-        <p>I worked on 5+ projects, including health care and blockchain projects.</p>
+        <p>I worked on 5+ projects, including health care and blockchain projects</p>
         <p> - Blockchain-based medical data platform</p>
         <p>- Microservice architecture</p>
         <p>- Salesforce administration</p>
       </div>
     ),
- },
+  },
   {
     date: 'May 2015 - Jan 2018',
-    location: 'NETPUT',
+    location: 'NETPUT | Remote',
     title: 'Full Stack Engineer',
     content: (
       <p>
-        I started work as a full-stack engineer.
-        Built and maintained the recruiting platform using JavaScript and Typescript.
-        Managed CI/CD using Github and AWS.
+        I started work as a full-stack engineer. Built and maintained the recruiting platform using PHP, Mern Stack, Managed CI/CD using Github and AWS.
       </p>
     ),
- },
+  },
 ];
 
 /**
@@ -276,12 +282,12 @@ export const testimonial: TestimonialSection = {
       name: 'John Doe',
       text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
-   },
+    },
     {
       name: 'Alex Saga',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      text: 'I am realy happy to know him and it is a oppotunity for me. Thanks to god.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
-   },
+    },
   ],
 };
 
@@ -302,12 +308,12 @@ export const contact: ContactSection = {
       type: ContactType.Location,
       text: 'Terrel TX, US',
       // href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-   },
+    },
     {
       type: ContactType.Github,
       text: 'tbakerx',
       href: 'https://github.com/zacharymillerm',
-   },
+    },
   ],
 };
 
